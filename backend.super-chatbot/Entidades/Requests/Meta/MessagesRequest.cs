12610a529e9191f31856b3365087a54955873260
Adolfo Sales Pacheco <adolfo.pacheco@gmail.com>
2024-10-15 19:24:55 -0300

    ajuste webhook post

--- backend.super-chatbot/Entidades/Requests/Meta/MessagesRequest.cs
+++ backend.super-chatbot/Entidades/Requests/Meta/MessagesRequest.cs
@@ -2,11 +2,29 @@
 
 public class MessagesRequest
 {
-    public string Messaging_product { get; set; }
-    public Metadata Metadata { get; set; }
-    public Contact[] Contacts { get; set; }
-    public Message[] Messages { get; set; }
+    public Entry[] entry { get; set; }
+
 }
+public class Entry
+{
+    public string id { get; set; }
+    public Change[] changes { get; set; }
+}
+
+public class Change
+{
+    public Value value { get; set; }
+    public string field { get; set; }
+}
+
+public class Value
+{
+    public string messaging_product { get; set; }
+    public Metadata metadata { get; set; }
+    public Contact[] contacts { get; set; }
+    public Message[] messages { get; set; }
+}
+
 public class Metadata
 {
     public string display_phone_number { get; set; }
