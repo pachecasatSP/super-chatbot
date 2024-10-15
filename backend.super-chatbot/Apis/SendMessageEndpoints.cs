02fa8e78e9469e4000bf5d6a0259494f014eb33f
Adolfo Sales Pacheco <adolfo.pacheco@gmail.com>
2024-10-15 15:53:35 -0300

    log sendmessage

--- backend.super-chatbot/Apis/SendMessageEndpoints.cs
+++ backend.super-chatbot/Apis/SendMessageEndpoints.cs
@@ -2,6 +2,7 @@
 using backend.super_chatbot.Results;
 using backend.super_chatbot.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace backend.super_chatbot.Apis
 {
@@ -11,13 +12,16 @@ namespace backend.super_chatbot.Apis
         {
             routes.MapPost("/sendmessage", async ([FromServices] ILogger<Program> logger, [FromServices] IWebhookService service, HttpContext context, SendMessageRequest request) =>
             {
+
+                logger.LogInformation($"recebendo o request: {JsonConvert.SerializeObject(request)}");
+
                 await service.SendMessage(new Entidades.Requests.Meta.SendMessageRequest()
                 {
                     Text = new Entidades.Requests.Meta.Text()
                     {
                         body = request.Message
                     },
-                    To = $"55{request.NumeroDestino}"
+                    To = request.NumeroDestino
                 }, int.Parse(context.Items["clientId"]?.ToString()));
 
 
